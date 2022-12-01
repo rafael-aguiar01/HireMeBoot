7bc34cb5a53aa423b57f1471223de020c36582fa
Rafael Aguiar <rafael.a1@hotmail.com>
2022-12-01 11:07:49 -0300

    test: ensure return 400 if no message is provided

--- src/presentation/controllers/send/sender.spec.ts
+++ src/presentation/controllers/send/sender.spec.ts
@@ -5,7 +5,7 @@ import { SendController } from "./sender"
 describe('Send Controller', () => {
     test('Should return 400 if no cellphone is provided', async() => {
         const sendStub = {
-            cellphone: 'valid_celphone',
+            cellphone: 'valid_cellphone',
             message: 'valid_message'
         }
         const sut = new SendController(sendStub)
@@ -17,5 +17,20 @@ describe('Send Controller', () => {
         const httpResponse = await sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse).toEqual(badRequest(new MissingParamError('cellphone')))
+    })
+    test('Should return 400 if no message is provided', async() => {
+        const sendStub = {
+            cellphone: 'valid_cellphone',
+            message: 'valid_message'
+        }
+        const sut = new SendController(sendStub)
+        const httpRequest = {
+            body: {
+                cellphone: 'valid_cellphone',
+            }
+        }
+        const httpResponse = await sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse).toEqual(badRequest(new MissingParamError('message')))
     })
 })
