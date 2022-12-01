caba408658d608c5a510abb79a7d8c8e3357fd2d
Rafael Aguiar <rafael.a1@hotmail.com>
2022-12-01 20:36:52 -0300

    refactor: create makeSut

--- src/presentation/controllers/send/sender.spec.ts
+++ src/presentation/controllers/send/sender.spec.ts
@@ -1,36 +1,63 @@
-import { MissingParamError } from "../../errors"
-import { badRequest } from "../../helpers/http-helper"
-import { SendController } from "./sender"
+import { SendModel } from '../../../domain/models/sendModel'
+import { Send } from '../../../domain/usecases/sendMessage'
+import { MissingParamError } from '../../errors'
+import { badRequest } from '../../helpers/http-helper'
+import { SendController } from './sender'
 
 describe('Send Controller', () => {
-    test('Should return 400 if no cellphone is provided', async() => {
-        const sendStub = {
-            cellphone: 'valid_cellphone',
-            message: 'valid_message'
-        }
-        const sut = new SendController(sendStub)
-        const httpRequest = {
-            body: {
-                message: 'valid_message'
-            }
-        }
-        const httpResponse = await sut.handle(httpRequest)
-        expect(httpResponse.statusCode).toBe(400)
-        expect(httpResponse).toEqual(badRequest(new MissingParamError('cellphone')))
-    })
-    test('Should return 400 if no message is provided', async() => {
-        const sendStub = {
-            cellphone: 'valid_cellphone',
-            message: 'valid_message'
-        }
-        const sut = new SendController(sendStub)
-        const httpRequest = {
-            body: {
-                cellphone: 'valid_cellphone',
-            }
-        }
-        const httpResponse = await sut.handle(httpRequest)
-        expect(httpResponse.statusCode).toBe(400)
-        expect(httpResponse).toEqual(badRequest(new MissingParamError('message')))
-    })
+  const makeFakeSendMessage = (): SendModel => ({
+    cellphone: 'valid_cellphone',
+    message: 'valid_message'
+  })
+
+  const makeSendMessage = (): Send => {
+    class SendMessageStub implements Send {
+      async send (send: SendModel): Promise<any> {
+        return new Promise(resolve => resolve(makeFakeSendMessage()))
+      }
+    }
+    return new SendMessageStub()
+  }
+  interface SutTypes{
+    sut: SendController
+    sendMessageStub: Send
+  }
+  const makeSut = (): SutTypes => {
+    const sendMessageStub = makeSendMessage()
+    const sut = new SendController(sendMessageStub)
+    return {
+      sut,
+      sendMessageStub
+    }
+  }
+  test('Should return 400 if no cellphone is provided', async () => {
+    const sendStub = {
+      cellphone: 'valid_cellphone',
+      message: 'valid_message'
+    }
+    const sut = new SendController(sendStub)
+    const httpRequest = {
+      body: {
+        message: 'valid_message'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('cellphone')))
+  })
+  test('Should return 400 if no message is provided', async () => {
+    const sendStub = {
+      cellphone: 'valid_cellphone',
+      message: 'valid_message'
+    }
+    const sut = new SendController(sendStub)
+    const httpRequest = {
+      body: {
+        cellphone: 'valid_cellphone'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('message')))
+  })
 })
--- src/presentation/controllers/send/sender.ts
+++ src/presentation/controllers/send/sender.ts
@@ -1,27 +1,25 @@
-import { Controller } from "../../protocols/controller";
-import { HttpRequest, HttpResponse } from "../../protocols/http";
+import { Controller } from '../../protocols/controller'
+import { HttpRequest, HttpResponse } from '../../protocols/http'
 import { SendModel } from '../../../domain/models/sendModel'
-import { badRequest } from "../../helpers/http-helper";
-import { MissingParamError } from "../../errors";
+import { badRequest } from '../../helpers/http-helper'
+import { MissingParamError } from '../../errors'
 
 export class SendController implements Controller {
-    private readonly sendData: SendModel
+  private readonly sendData: SendModel
 
-    constructor (sendData: SendModel){
-        this.sendData = sendData
-    } 
+  constructor (sendData: SendModel) {
+    this.sendData = sendData
+  }
 
-    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const requiredFields = [
-            'cellphone',
-            'message'
-        ]
-        for (const field of requiredFields){
-            if(!httpRequest.body[field]) {
-                return badRequest(new MissingParamError(field))
-            }
-        }
-
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const requiredFields = [
+      'cellphone',
+      'message'
+    ]
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
-
+  }
 }
