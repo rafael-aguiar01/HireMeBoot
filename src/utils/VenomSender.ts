4985ea8ac29a92a181d459471c97c54d087a3a3a
Rafael Aguiar <rafael.a1@hotmail.com>
2022-12-01 11:45:05 -0300

    feat: create venom send message

---
+++ src/utils/VenomSender.ts
@@ -0,0 +1,17 @@
+export class Send {
+    private readonly client
+    private readonly number
+
+    constructor (client: any, chatId: any ){
+        this.client = client
+        this.number = chatId
+    } 
+    async sendText(to :string, message :string) {
+        try {
+            await this.client.sendText(this.number(to), message)
+        } catch (error) {
+            console.log(error)
+            return error
+        }
+    }
+} 
