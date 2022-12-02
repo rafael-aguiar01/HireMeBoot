9a451e79779d4a8808c97f4e222191d01b08290f
Rafael Aguiar <rafael.a1@hotmail.com>
2022-12-02 09:31:35 -0300

    feat: return 400 if invalid cellphone is provided

--- src/presentation/controllers/send/sender.ts
+++ src/presentation/controllers/send/sender.ts
@@ -2,13 +2,16 @@ import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 import { SendModel } from '../../../domain/models/sendModel'
 import { badRequest } from '../../helpers/http-helper'
-import { MissingParamError } from '../../errors'
+import { MissingParamError, InvalidParamError } from '../../errors'
+import { CellphoneValidator } from '../../protocols/cellphone-validator'
 
 export class SendController implements Controller {
   private readonly sendData: SendModel
+  private readonly phoneNumberValidator: CellphoneValidator
 
-  constructor (sendData: SendModel) {
+  constructor (sendData: SendModel, phoneNumberValidator: CellphoneValidator) {
     this.sendData = sendData
+    this.phoneNumberValidator = phoneNumberValidator
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -20,6 +23,12 @@ export class SendController implements Controller {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+    const { cellphone } = httpRequest.body
+
+    const isValid = this.phoneNumberValidator.isValid(cellphone)
+    if (!isValid) {
+      return badRequest(new InvalidParamError(cellphone))
     }
   }
 }
