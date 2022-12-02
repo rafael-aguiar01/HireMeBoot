c3abdde6ae07bf4fb17036b2d667d2aa5276d6f3
Rafael Aguiar <rafael.a1@hotmail.com>
2022-12-02 08:15:20 -0300

    test: return a correct number on success

---
+++ src/infra/ParsePhoneNumber/libphonenumber-adapter.spec.ts
@@ -0,0 +1,15 @@
+import { LibPhoneNumberAdapter } from './libphonenumber-adapter'
+
+const makeSut = (): LibPhoneNumberAdapter => {
+  return new LibPhoneNumberAdapter()
+}
+const validPhoneNumber = '11977805388'
+const parsePhoneNumber = '5511977805388'
+
+describe('Libphonenumber Adapter', () => {
+  test('Should return a correct number on success', async () => {
+    const sut = makeSut()
+    const parseReturn = await sut.parse(validPhoneNumber)
+    expect(parseReturn).toBe(parsePhoneNumber)
+  })
+})
