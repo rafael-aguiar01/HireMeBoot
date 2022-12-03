da41d62fe63a0276c1ada89c22caf5a2a4e9dff2
Rafael Aguiar <rafael.a1@hotmail.com>
2022-12-03 12:19:43 -0300

    refactor: move start to ServiceBoot

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,53 +1,19 @@
 import express from 'express'
 import { create } from 'venom-bot'
-// import { create, Whatsapp } from 'venom-bot'
-// import { Send } from '../infra/VenomBot/venom-sender'
-import { start } from './start'
+import { ServiceBoot } from './start'
 
 const app = express()
 const port = 5050
 app.use(express.json())
 
 create({
-  session: 'HireMeBoot',
-  multidevice: true
+  session: 'HireMeBoot'
 }).then((client) => {
-  start(client)
+  ServiceBoot.setInstance(client)
+  ServiceBoot.start()
 }).catch((error) => {
   console.error(error)
 })
-
-// create({
-//   session: 'HireMeBoot',
-//   multidevice: true
-// }).then((client) => {
-//   start(client)
-//   app.post('/send', async (req, res) => {
-//     const { cellphone, message } = req.body
-//     const send = new Send(client)
-//     const ok = await send.sendText(cellphone, message)
-//     res.send(ok)
-//   })
-// })
-//   .catch((erro) => {
-
-//   })
-
-// function start (client): any {
-//   client.onMessage((message) => {
-//     if (message.isGroupMsg === false) {
-//       client
-//         .sendText(message.from, 'Olá eu sou o HireMeBoot')
-//         .then((result) => {
-//           console.log('Result: ', result)
-//           return client
-//         })
-//         .catch((erro) => {
-//           console.error('Error when sending: ', erro)
-//         })
-//     }
-//   })
-// }
 
 app.listen(port, () => {
   console.log(`HireMeBoot listening on port ${port}`)
