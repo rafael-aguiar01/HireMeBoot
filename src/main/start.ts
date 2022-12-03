9524e032413115eacdecf1a5aba266704280cf89
Rafael Aguiar <rafael.a1@hotmail.com>
2022-12-03 14:05:47 -0300

    refactor: move start function to single file

--- src/main/start.ts
+++ src/main/start.ts
@@ -1,11 +1,13 @@
-export function start (client): any {
+import { ServiceBoot } from './serviceBoot'
+
+export async function start () {
+  const client = await ServiceBoot.client
   client.onMessage((message) => {
     if (message.isGroupMsg === false) {
       client
         .sendText(message.from, 'Olá eu sou o HireMeBoot')
         .then((result) => {
           console.log('Result: ', result)
-          return client
         })
         .catch((erro) => {
           console.error('Error when sending: ', erro)
